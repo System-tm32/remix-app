d884101497055c6dc351609b128ed428b7e7df81
System-tm32 <systemtm32@yandex.ru>
2023-08-28 16:02:58 +0300

    chore: add dependencies

--- app/routes/hookah/index.tsx
+++ app/routes/hookah/index.tsx
@@ -1,8 +1,13 @@
-import type { LinksFunction, V2_MetaFunction } from "@remix-run/node";
+import {
+  json,
+  type LinksFunction,
+  type V2_MetaFunction,
+} from "@remix-run/node";
 import styles from "./styles.css";
 import { cssBundleHref } from "@remix-run/css-bundle";
-import { Link, Outlet } from "@remix-run/react";
+import { Link, Outlet, useLoaderData } from "@remix-run/react";
 import MainMenu from "~/components/core/menu";
+import { prisma } from "~/db.server";
 
 export const links: LinksFunction = () => [
   { rel: "stylesheet", href: styles },
@@ -11,10 +16,21 @@ export const links: LinksFunction = () => [
 
 export const meta: V2_MetaFunction = () => [{ title: "Hookah Page" }];
 
+export const loader = async () => {
+  return json(await prisma.user.findMany());
+};
+
 export default function HomePage() {
+  const users = useLoaderData<typeof loader>();
+  console.log(users);
   return (
     <main className="relativ h-full w-full flex flex-row">
       <div className="content grow">
+        <ul>
+          {users.map((user) => (
+            <li key={user.id}>{user.email}</li>
+          ))}
+        </ul>
         <Link to={"recipe"}>Go recipe</Link>
         <br />
         <Link to={"/hookah"}>Back</Link>
